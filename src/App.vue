e1f4e8ef9724761690e126a58f0eb18ce05293d6
roliiii <roli4759@hotmail.com>
2020-05-19 00:12:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }, 
+  data(){
+    return {
+      todos:[
+        {id:1,title:'todo1',completed:true},
+        {id:2,title:'todo2',completed:true},
+        {id:3,title:'todo3',completed:false},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+ * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="(todo, index) in todos" >
+            <h3>{{todo.title}} - {{index}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
