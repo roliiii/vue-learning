8db01fc6a5c0037cbd144a6d5884bdfa43516a2d
roliiii <roli4759@hotmail.com>
2020-05-27 14:47:17 +0200

    add todos

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,63 @@
 <template>
   <div id="app">
     <Header />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
-import Header from './components/layout/Header.vue'
+import Todos from "./components/Todos.vue";
+import Header from "./components/layout/Header.vue";
+import AddTodo from "./components/AddTodo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
+    AddTodo,
     Todos
-  }, 
-  data(){
-    return {
-      todos:[
-        {id:1,title:'todo1',completed:true},
-        {id:2,title:'todo2',completed:true},
-        {id:3,title:'todo3',completed:false},
-      ]
-    }
   },
-  methods:{
-    deleteTodo(id){
-      this.todos = this.todos.filter(x => x.id!=id)
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "todo1", completed: true },
+        { id: 2, title: "todo2", completed: true },
+        { id: 3, title: "todo3", completed: false }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(x => x.id != id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo]
     }
   }
-}
+};
 </script>
 
 <style>
- * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-bloPck;
+  border: none;
+  background: #555;
+  color: white;
+  cursor: pointer;
+  padding: 7px 20px;
+}
+
+.btn:hover {
+  background: #666;
+}
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,7 @@
   <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
       <p> 
         <input type="checkbox" v-on:change="markComplete">
-        {{todo.title}} - {{index}}
+        {{todo.title}}
         <button @click="$emit('del-todo', todo.id)" class="del">X</button>
       </p>
   </div>
